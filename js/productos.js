e0c9d6188dcfa0259fe6b58abd2ea83976ec5091
Sebastian Almendra <salmendra@azurian.com>
2024-09-01 21:23:38 -0400

    ejemplo de fetch

---
+++ js/productos.js
@@ -0,0 +1,27 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Almendras En Cascara",
+        precio: 2500,
+    },
+    {
+        id: 2,
+        nombre: "Almendras Enteras",
+        precio: 8000,
+    },
+    {
+        id: 3,
+        nombre: "Almendras Partidas",
+        precio: 7000,
+    },
+    {
+        id: 4,
+        nombre: "Nueces Mariposa",
+        precio: 8500,
+    },
+    {
+        id: 5,
+        nombre: "Nueces Cuarto",
+        precio: 7000,
+    }
+]
